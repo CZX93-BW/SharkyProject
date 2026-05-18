497a6c681b585b8517d6a1120fb551d033d1300c
CZX93-BW <yunimoon2012@gmail.com>
2026-05-18 15:47:00 +0200

    feat(input): improve keyboard handling

--- js/classes/game-state.class.js
+++ js/classes/game-state.class.js
@@ -6,6 +6,7 @@ class GameState {
         this.isPaused = false;
         this.currentLevel = 1;
         this.coins = 0;
+        this.framesPerSecond = 0;
         this.debugMode = this.getDebugMode();
         this.player = this.createPlayer();
     }
@@ -16,28 +17,27 @@ class GameState {
     }
 
     createPlayer() {
-        return {
-            x: GAME_CONFIG.playerStartX,
-            y: GAME_CONFIG.playerStartY,
-            width: GAME_CONFIG.playerWidth,
-            height: GAME_CONFIG.playerHeight,
-            speed: GAME_CONFIG.playerSpeed
-        };
+        return new Character();
     }
 
     start(levelNumber) {
         this.currentLevel = levelNumber;
         this.isRunning = true;
         this.isPaused = false;
+        this.resetCoins();
         this.resetPlayer();
     }
 
     pause() {
-        this.isPaused = true;
+        if (this.isRunning) {
+            this.isPaused = true;
+        }
     }
 
     resume() {
-        this.isPaused = false;
+        if (this.isRunning) {
+            this.isPaused = false;
+        }
     }
 
     stop() {
@@ -47,5 +47,13 @@ class GameState {
 
     resetPlayer() {
         this.player = this.createPlayer();
+    }
+
+    resetCoins() {
+        this.coins = 0;
+    }
+
+    setFramesPerSecond(framesPerSecond) {
+        this.framesPerSecond = framesPerSecond;
     }
 }
--- js/classes/keyboard.class.js
+++ js/classes/keyboard.class.js
@@ -3,16 +3,52 @@
 class Keyboard {
     constructor() {
         this.pressedKeys = {};
+        this.gameKeys = this.createGameKeys();
         this.bindKeyboardEvents();
     }
 
-    bindKeyboardEvents() {
-        window.addEventListener('keydown', (event) => this.setKeyState(event, true));
-        window.addEventListener('keyup', (event) => this.setKeyState(event, false));
+    createGameKeys() {
+        return [
+            'ArrowLeft',
+            'ArrowRight',
+            'ArrowUp',
+            'ArrowDown',
+            'KeyA',
+            'KeyD',
+            'KeyW',
+            'KeyS',
+            'Space',
+            'KeyF'
+        ];
     }
 
-    setKeyState(event, isPressed) {
-        this.pressedKeys[event.code] = isPressed;
+    bindKeyboardEvents() {
+        window.addEventListener('keydown', (event) => this.handleKeyDown(event));
+        window.addEventListener('keyup', (event) => this.handleKeyUp(event));
+    }
+
+    handleKeyDown(event) {
+        this.preventBrowserMovement(event);
+        this.setKeyState(event.code, true);
+    }
+
+    handleKeyUp(event) {
+        this.preventBrowserMovement(event);
+        this.setKeyState(event.code, false);
+    }
+
+    preventBrowserMovement(event) {
+        if (this.isGameKey(event.code)) {
+            event.preventDefault();
+        }
+    }
+
+    isGameKey(keyCode) {
+        return this.gameKeys.includes(keyCode);
+    }
+
+    setKeyState(keyCode, isPressed) {
+        this.pressedKeys[keyCode] = isPressed;
     }
 
     isMovingLeft() {
@@ -29,5 +65,13 @@ class Keyboard {
 
     isMovingDown() {
         return this.pressedKeys.ArrowDown || this.pressedKeys.KeyS;
+    }
+
+    isBubbleAttackPressed() {
+        return this.pressedKeys.Space;
+    }
+
+    isPoisonAttackPressed() {
+        return this.pressedKeys.KeyF;
     }
 }
