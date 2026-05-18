03b1365c8395f6d47bc70470ddf0cb37c6e1eb56
CZX93-BW <yunimoon2012@gmail.com>
2026-05-18 18:54:06 +0200

    chore(world): add level configuration files

---
+++ js/config/asset-config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const ASSET_CONFIG = {
+    backgrounds: {
+        levelOne: {
+            back: '',
+            middle: '',
+            front: ''
+        },
+        levelTwo: {
+            back: '',
+            middle: '',
+            front: ''
+        }
+    }
+};
