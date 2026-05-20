1a179f62a319e6690200ad4b80f1b21e41709f9e
CZX93-BW <yunimoon2012@gmail.com>
2026-05-20 09:42:34 +0200

    feat(collision): add collectible collisions

--- js/classes/collision-manager.class.js
+++ js/classes/collision-manager.class.js
@@ -6,10 +6,45 @@ class CollisionManager {
     }
 
     checkPlayerEnemyCollision(player, enemy) {
-        if (!player.isCollidingWith(enemy)) {
+        if (!this.isOverlapping(player, enemy)) {
             return;
         }
 
         player.takeDamage(enemy.damage);
+    }
+
+    checkPlayerCollectibleCollisions(gameState) {
+        gameState.activeLevel.collectibles.forEach((collectible) => {
+            this.checkPlayerCollectibleCollision(gameState, collectible);
+        });
+    }
+
+    checkPlayerCollectibleCollision(gameState, collectible) {
+        if (!this.canCollect(gameState.player, collectible)) {
+            return;
+        }
+
+        this.applyCollectible(gameState, collectible);
+        collectible.collect();
+    }
+
+    canCollect(player, collectible) {
+        return !collectible.isCollected && this.isOverlapping(player, collectible);
+    }
+
+    applyCollectible(gameState, collectible) {
+        if (collectible.type === 'coin') {
+            gameState.collectCoin(collectible.value);
+            return;
+        }
+
+        gameState.collectPoisonBottle(collectible.value);
+    }
+
+    isOverlapping(firstObject, secondObject) {
+        return firstObject.getRightSide() > secondObject.x &&
+            firstObject.x < secondObject.getRightSide() &&
+            firstObject.getBottomSide() > secondObject.y &&
+            firstObject.y < secondObject.getBottomSide();
     }
 }
