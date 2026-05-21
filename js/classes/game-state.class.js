3ca6ee3a3e572a0da596b8f66a7fa99b2246e78f
CZX93-BW <yunimoon2012@gmail.com>
2026-05-21 23:49:07 +0200

    feat(shop): add upgrade purchase logic

--- js/classes/game-state.class.js
+++ js/classes/game-state.class.js
@@ -9,6 +9,7 @@ class GameState {
         this.activeLevel = this.getLevelByNumber(1);
         this.coins = 0;
         this.poisonBottles = 0;
+        this.upgrades = this.createDefaultUpgrades();
         this.framesPerSecond = 0;
         this.debugMode = this.getDebugMode();
         this.player = this.createPlayer();
@@ -27,15 +28,41 @@ class GameState {
         return new Character();
     }
 
+    createDefaultUpgrades() {
+        return {
+            speedBoost: false,
+            extraHealth: false,
+            poisonCapacity: false
+        };
+    }
+
     start(levelNumber) {
+        this.resetSession();
+        this.startLevel(levelNumber);
+    }
+
+    startNextLevel(levelNumber) {
+        this.startLevel(levelNumber);
+    }
+
+    restartCurrentLevel() {
+        this.startLevel(this.currentLevel);
+    }
+
+    startLevel(levelNumber) {
         this.currentLevel = levelNumber;
         this.activeLevel = this.getLevelByNumber(levelNumber);
         this.status = 'playing';
         this.isRunning = true;
         this.isPaused = false;
-        this.resetResources();
         this.resetPlayer();
         this.resetLevel();
+    }
+
+    resetSession() {
+        this.coins = 0;
+        this.poisonBottles = 0;
+        this.upgrades = this.createDefaultUpgrades();
     }
 
     pause() {
@@ -59,9 +86,17 @@ class GameState {
     }
 
     completeLevel() {
-        this.status = 'levelComplete';
+        this.status = this.getCompletionStatus();
         this.isRunning = false;
         this.isPaused = false;
+    }
+
+    getCompletionStatus() {
+        if (this.currentLevel === 1) {
+            return 'shop';
+        }
+
+        return 'levelComplete';
     }
 
     setGameOver() {
@@ -72,15 +107,32 @@ class GameState {
 
     resetPlayer() {
         this.player = this.createPlayer();
+        this.applyUpgradesToPlayer();
+    }
+
+    applyUpgradesToPlayer() {
+        this.applySpeedUpgrade();
+        this.applyHealthUpgrade();
+    }
+
+    applySpeedUpgrade() {
+        if (this.upgrades.speedBoost) {
+            this.player.increaseSpeed(GAME_CONFIG.upgradeSpeedBonus);
+        }
+    }
+
+    applyHealthUpgrade() {
+        if (this.upgrades.extraHealth) {
+            this.player.setMaxHealth(this.getUpgradedHealth());
+        }
+    }
+
+    getUpgradedHealth() {
+        return GAME_CONFIG.playerHealth + GAME_CONFIG.upgradeHealthBonus;
     }
 
     resetLevel() {
         this.activeLevel.reset();
-    }
-
-    resetResources() {
-        this.coins = 0;
-        this.poisonBottles = 0;
     }
 
     collectCoin(value) {
@@ -89,7 +141,44 @@ class GameState {
 
     collectPoisonBottle(value) {
         const nextValue = this.poisonBottles + value;
-        this.poisonBottles = Math.min(nextValue, GAME_CONFIG.playerMaxPoisonBottles);
+        this.poisonBottles = Math.min(nextValue, this.getMaxPoisonBottles());
+    }
+
+    getMaxPoisonBottles() {
+        if (this.upgrades.poisonCapacity) {
+            return GAME_CONFIG.playerMaxPoisonBottles + GAME_CONFIG.upgradePoisonCapacityBonus;
+        }
+
+        return GAME_CONFIG.playerMaxPoisonBottles;
+    }
+
+    purchaseUpgrade(upgradeName) {
+        if (!this.canPurchaseUpgrade(upgradeName)) {
+            return false;
+        }
+
+        this.coins -= this.getUpgradeCost(upgradeName);
+        this.upgrades[upgradeName] = true;
+        this.applyUpgradesToPlayer();
+        return true;
+    }
+
+    canPurchaseUpgrade(upgradeName) {
+        return this.hasUpgradeConfig(upgradeName) &&
+            !this.upgrades[upgradeName] &&
+            this.coins >= this.getUpgradeCost(upgradeName);
+    }
+
+    hasUpgradeConfig(upgradeName) {
+        return Boolean(GAME_CONFIG.shopUpgrades[upgradeName]);
+    }
+
+    getUpgradeCost(upgradeName) {
+        return GAME_CONFIG.shopUpgrades[upgradeName].cost;
+    }
+
+    isUpgradeOwned(upgradeName) {
+        return Boolean(this.upgrades[upgradeName]);
     }
 
     setFramesPerSecond(framesPerSecond) {
