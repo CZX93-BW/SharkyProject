9abf7ffb66ecb65ae53cc6b072ad68da9f1c6ec6
CZX93-BW <yunimoon2012@gmail.com>
2026-05-22 06:44:03 +0200

    feat(input): support mobile input state

--- js/classes/keyboard.class.js
+++ js/classes/keyboard.class.js
@@ -3,8 +3,24 @@
 class Keyboard {
     constructor() {
         this.pressedKeys = {};
+        this.mobileMovement = this.createMobileMovement();
+        this.mobileActions = this.createMobileActions();
         this.gameKeys = this.createGameKeys();
         this.bindKeyboardEvents();
+    }
+
+    createMobileMovement() {
+        return {
+            x: 0,
+            y: 0
+        };
+    }
+
+    createMobileActions() {
+        return {
+            bubble: false,
+            poison: false
+        };
     }
 
     createGameKeys() {
@@ -51,27 +67,68 @@ class Keyboard {
         this.pressedKeys[keyCode] = isPressed;
     }
 
+    setMobileMovement(x, y) {
+        this.mobileMovement.x = x;
+        this.mobileMovement.y = y;
+    }
+
+    resetMobileMovement() {
+        this.mobileMovement = this.createMobileMovement();
+    }
+
+    setMobileAction(actionName, isPressed) {
+        this.mobileActions[actionName] = isPressed;
+    }
+
     isMovingLeft() {
-        return this.pressedKeys.ArrowLeft || this.pressedKeys.KeyA;
+        return this.isKeyPressed('ArrowLeft') ||
+            this.isKeyPressed('KeyA') ||
+            this.isMobileMovingLeft();
     }
 
     isMovingRight() {
-        return this.pressedKeys.ArrowRight || this.pressedKeys.KeyD;
+        return this.isKeyPressed('ArrowRight') ||
+            this.isKeyPressed('KeyD') ||
+            this.isMobileMovingRight();
     }
 
     isMovingUp() {
-        return this.pressedKeys.ArrowUp || this.pressedKeys.KeyW;
+        return this.isKeyPressed('ArrowUp') ||
+            this.isKeyPressed('KeyW') ||
+            this.isMobileMovingUp();
     }
 
     isMovingDown() {
-        return this.pressedKeys.ArrowDown || this.pressedKeys.KeyS;
+        return this.isKeyPressed('ArrowDown') ||
+            this.isKeyPressed('KeyS') ||
+            this.isMobileMovingDown();
+    }
+
+    isKeyPressed(keyCode) {
+        return Boolean(this.pressedKeys[keyCode]);
+    }
+
+    isMobileMovingLeft() {
+        return this.mobileMovement.x < -GAME_CONFIG.mobileJoystickThreshold;
+    }
+
+    isMobileMovingRight() {
+        return this.mobileMovement.x > GAME_CONFIG.mobileJoystickThreshold;
+    }
+
+    isMobileMovingUp() {
+        return this.mobileMovement.y < -GAME_CONFIG.mobileJoystickThreshold;
+    }
+
+    isMobileMovingDown() {
+        return this.mobileMovement.y > GAME_CONFIG.mobileJoystickThreshold;
     }
 
     isBubbleAttackPressed() {
-        return this.pressedKeys.Space;
+        return this.isKeyPressed('Space') || this.mobileActions.bubble;
     }
 
     isPoisonAttackPressed() {
-        return this.pressedKeys.KeyF;
+        return this.isKeyPressed('KeyF') || this.mobileActions.poison;
     }
 }
