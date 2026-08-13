bddfdb8ceee01c40826a3cf2f71d18c2bf7f67f5
CZX93-BW <yunimoon2012@gmail.com>
2026-08-13 20:11:36 +0200

    feat(assets): connect level one background layers

--- js/config/asset-config.js
+++ js/config/asset-config.js
@@ -6,11 +6,11 @@ const ASSET_CONFIG = {
     },
     backgrounds: {
         levelOne: {
-            far: '',
-            back: '',
-            middle: '',
-            front: '',
-            floor: ''
+            far: 'assets/img/backgrounds/Layers/5. Water/water_complete_lvl_1.png',
+            back: 'assets/img/backgrounds/Layers/4.Fondo 2/fondo_complete_lvl_1_v2.png',
+            middle: 'assets/img/backgrounds/Layers/3.Fondo 1/fondo_complete_lvl_1.png',
+            front: 'assets/img/backgrounds/Layers/1. Light/complete_light.png',
+            floor: 'assets/img/backgrounds/Layers/2. Floor/floor0_complete_lvl_1.png'
         },
         levelTwo: {
             far: '',
