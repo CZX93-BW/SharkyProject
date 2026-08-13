aa1aea94da0c990b9146a9847de4e927d4f694b9
CZX93-BW <yunimoon2012@gmail.com>
2026-08-13 20:18:38 +0200

    feat(assets): connect level two background layers

--- js/config/asset-config.js
+++ js/config/asset-config.js
@@ -13,11 +13,11 @@ const ASSET_CONFIG = {
             floor: 'assets/img/backgrounds/Layers/2. Floor/floor0_complete_lvl_1.png'
         },
         levelTwo: {
-            far: '',
-            back: '',
-            middle: '',
-            front: '',
-            floor: ''
+            far: 'assets/img/backgrounds/Layers/5. Water/water_complete_lvl_2.png',
+            back: 'assets/img/backgrounds/Layers/4.Fondo 2/fondo_complete_lvl_2_v2.png',
+            middle: 'assets/img/backgrounds/Layers/3.Fondo 1/fondo_complete_lvl_2.png',
+            front: 'assets/img/backgrounds/Layers/1. Light/complete_light.png',
+            floor: 'assets/img/backgrounds/Layers/2. Floor/floor0_complete_lvl_2.png'
         }
     },
     enemies: {
