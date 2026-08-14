f5dcaa41adb18d5ee56deed9d03f986934a81040
CZX93-BW <yunimoon2012@gmail.com>
2026-08-14 18:28:51 +0200

    feat(assets): define Endboss floating hurt and dead frames

--- js/config/asset-config.js
+++ js/config/asset-config.js
@@ -130,7 +130,39 @@ const ASSET_CONFIG = {
             ]
         },
 
-        endboss: ''
+        endboss: {
+            floating: [
+                'assets/img/enemies/3 Final Enemy/2.floating/1.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/2.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/3.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/4.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/5.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/6.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/7.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/8.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/9.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/10.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/11.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/12.png',
+                'assets/img/enemies/3 Final Enemy/2.floating/13.png'
+            ],
+
+            hurt: [
+                'assets/img/enemies/3 Final Enemy/Hurt/1.png',
+                'assets/img/enemies/3 Final Enemy/Hurt/2.png',
+                'assets/img/enemies/3 Final Enemy/Hurt/3.png',
+                'assets/img/enemies/3 Final Enemy/Hurt/4.png'
+            ],
+
+            dead: [
+                'assets/img/enemies/3 Final Enemy/Dead/Mesa de trabajo 2.png',
+                'assets/img/enemies/3 Final Enemy/Dead/Mesa de trabajo 2 copia 6.png',
+                'assets/img/enemies/3 Final Enemy/Dead/Mesa de trabajo 2 copia 7.png',
+                'assets/img/enemies/3 Final Enemy/Dead/Mesa de trabajo 2 copia 8.png',
+                'assets/img/enemies/3 Final Enemy/Dead/Mesa de trabajo 2 copia 9.png',
+                'assets/img/enemies/3 Final Enemy/Dead/Mesa de trabajo 2 copia 10.png'
+            ]
+        }
     },
 
     collectibles: {
