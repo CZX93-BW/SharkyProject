bbbd7cfabd237c69f399281cb9276ca9f67031f8
CZX93-BW <yunimoon2012@gmail.com>
2026-08-17 20:37:28 +0200

    feat(assets): register pufferfish state animations

--- js/config/asset-config.js
+++ js/config/asset-config.js
@@ -117,6 +117,22 @@ const ASSET_CONFIG = {
                 'assets/img/enemies/1.Puffer fish (3 color options)/1.Swim/1.swim5.png'
             ],
 
+            transition: [
+                'assets/img/enemies/1.Puffer fish (3 color options)/2.transition/1.transition1.png',
+                'assets/img/enemies/1.Puffer fish (3 color options)/2.transition/1.transition2.png',
+                'assets/img/enemies/1.Puffer fish (3 color options)/2.transition/1.transition3.png',
+                'assets/img/enemies/1.Puffer fish (3 color options)/2.transition/1.transition4.png',
+                'assets/img/enemies/1.Puffer fish (3 color options)/2.transition/1.transition5.png'
+            ],
+
+            inflatedSwim: [
+                'assets/img/enemies/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim1.png',
+                'assets/img/enemies/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim2.png',
+                'assets/img/enemies/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim3.png',
+                'assets/img/enemies/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim4.png',
+                'assets/img/enemies/1.Puffer fish (3 color options)/3.Bubbleeswim/1.bubbleswim5.png'
+            ],
+
             dead: [
                 'assets/img/enemies/1.Puffer fish (3 color options)/4.DIE/1.Dead 1 (can animate by going up).png',
                 'assets/img/enemies/1.Puffer fish (3 color options)/4.DIE/1.Dead 2 (can animate by going down to the floor after the Fin Slap attack).png',
